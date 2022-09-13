b833e0563b9d28ab0305daa0a193f9de10bd9982
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-09-13 11:10:08 +0200

    Lesson 5
    Lesson 5 exercise 1 and 2 done. 3 is almost done

---
+++ Bjarke/Lesson_5/Lesson_5/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AgentAssignment1;
+
+namespace Lesson_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Agent agent = new Agent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            agent.ID = "007";
+            agent.CodeName = "James Bond";
+            agent.Speciality = "Assassionation";
+            agent.Assignment = "UpperVolta";
+            DataContext = agent;
+        }
+    }
+}
---
+++ Bjarke/Lesson_5/Lesson_5_2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Lesson_5_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        List<Agent> agents = new List<Agent>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            agents.Add(new Agent("001", "Dr Drake", "Assassionation", "Goldfinger"));
+            agents.Add(new Agent("007", "James Bond", "Assassionation", "UpperVolta"));
+
+            DataContext = agents;
+
+        }
+    }
+}
