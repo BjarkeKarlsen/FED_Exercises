a9cbafa19b376ac8fc6da49a3d37dd593b6c69d4
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-09-14 09:27:41 +0200

    Lesson_5_3
    Has a mistake when you click add new agent and don't show any agent.

--- Bjarke/Lesson_5/Lesson_5_3/MainWindow.xaml.cs
+++ Bjarke/Lesson_5/Lesson_5_3/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AgentAssignment1;
 
 namespace Lesson_5_3
 {
@@ -20,9 +24,124 @@ namespace Lesson_5_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Agent> agents;
         public MainWindow()
         {
             InitializeComponent();
+            agents = new ObservableCollection<Agent>();
+            agents.Add(new Agent("001", "Dr Drake", "Assassionation", "Goldfinger"));
+            agents.Add(new Agent("007", "James Bond", "Assassionation", "UpperVolta"));
+
+            CurrentAgent = agents[0];
         }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxAgents.SelectedIndex > 0)
+                lbxAgents.SelectedIndex = --lbxAgents.SelectedIndex;
+        }
+
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxAgents.SelectedIndex < lbxAgents.Items.Count - 1)
+                lbxAgents.SelectedIndex = ++lbxAgents.SelectedIndex;
+        }
+
+        private void BtnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as MainWindow;
+            vm.AddNewAgent();
+            lbxAgents.SelectedIndex = lbxAgents.Items.Count - 1;
+            tbxId.Focus();
+        }
+
+
+        #region Propeties
+
+        private Agent currentAgent = null;
+        public Agent CurrentAgent
+        {
+            get { return currentAgent; }
+            set
+            {
+                if (currentAgent != value)
+                {
+                    currentAgent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+        }
+
+        public ObservableCollection<Agent> Agents
+        {
+            get { return agents; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddNewAgent()
+        {
+            agents.Add(new Agent());
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+
+
+        //#region Propeties
+
+        //public Agent CurrentAgent : NotifyPropertyChanged
+        //{
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //Agent currentAgent
+        //{
+        //    get { return currentAgent; }
+        //    set
+        //    {
+        //        if (currentAgent != value)
+        //        {
+        //            currentAgent = value;
+        //            NotifyPropertyChanged();
+        //        }
+        //    }
+
+        //}
+
+        //public ObservableCollection<Agent> Agents
+        //{
+        //    get { return agents; }
+        //}
+        //#endregion
+
+        //#region Methods
+        //public void AddNewAgent()
+        //{
+        //    agents.Add(new Agent());
+        //}
+
+        //#endregion
+
+
+
     }
 }
